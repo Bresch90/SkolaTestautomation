cb515343f3d5d6edafd8497e058c40b92ff84ce8
Bresch90 <92075441+Bresch90@users.noreply.github.com>
2023-02-16 21:11:49 +0100

    fixat test_search_exact och börjat med test_add_to_cart

--- Tests för Kjell o Company/test_Kjell.py
+++ Tests för Kjell o Company/test_Kjell.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import pytest
 from time import sleep
@@ -12,6 +13,7 @@ browser = 'chrome'
 @pytest.fixture(autouse=True, scope='class')
 def driver(request):
     global browser
+    # needed a global variable since it can only be fetched once it seems.
     browser = request.config.getoption('--browser').lower()
 
     match browser:
@@ -30,14 +32,20 @@ def driver(request):
 
 
 def wait_and_click(active_driver, path):
-    WebDriverWait(active_driver, timeout=5).until(lambda d: d.find_element(By.XPATH, path))
-    element = active_driver.find_element(By.XPATH, path)
-    element.click()
+    WebDriverWait(active_driver, timeout=5).until(EC.element_to_be_clickable((By.XPATH, path))).click()
 
 
 def wait_and_get_element(active_driver, path):
-    WebDriverWait(active_driver, timeout=5).until(lambda d: d.find_element(By.XPATH, path))
+    WebDriverWait(active_driver, timeout=5).until(EC.element_to_be_clickable((By.XPATH, path)))
     return active_driver.find_element(By.XPATH, path)
+
+
+def add_to_cart(active_driver):
+    # add to cart
+    wait_and_click(active_driver, "//*[@id='addToCart']")
+    # close overlay window
+    sleep(0.5)
+    wait_and_click(active_driver, "//div[4]/div[2]/div[2]/div/div/div[1]/button")
 
 
 class TestKjell:
@@ -67,6 +75,49 @@ class TestKjell:
         chosen_store = wait_and_get_element(driver, "//nav/div/div[9]/div[1]/div/div[2]")
         assert "kalmar" in chosen_store.text.lower()
 
+    @pytest.mark.skip(reason="test_search_exact: Not implemented on site. "
+                             "Does something with quotation but doesnt search for exact.")
+    def test_search_exact(self, driver):
+        driver.get(HOMEPAGE)
+        driver.maximize_window()  # menu button has different path if screen is too small.
+        search_bar = driver.find_element(By.XPATH, '//form/div[1]/input')
+        search_bar.send_keys("\"test\"", Keys.RETURN)
 
-# if __name__ == "__main__":
-#  pass
+        # get product from search results, needs wait?
+        sleep(1)
+        wait_and_get_element(driver, "//h3")
+        # "//div[5]/a/div/h3"
+        products_list = [e.text for e in driver.find_elements(By.XPATH, "//h3")]
+        assert "test" in products_list[0].lower()
+
+# not done yet..
+    def test_add_to_cart(self, driver):
+        item_names_list = []
+        prices_dict = {}
+        product_positions = [2, 4, 7]
+        driver.get(HOMEPAGE)
+        driver.maximize_window()  # menu button has different path if screen is too small.
+        search_bar = driver.find_element(By.XPATH, '//form/div[1]/input')
+        search_bar.send_keys("test", Keys.RETURN)
+        # get product from search results, needs wait?
+        sleep(1)
+        # collect data on the 2nd, 4th and 7th products
+        for pos in product_positions:
+            # name
+            item_names_list.append(wait_and_get_element(driver, f"//div[1]/div/div[{pos}]/a/div[2]/h3").text)
+            # price
+            price = float(wait_and_get_element(driver, f"//div[1]/div/div[{pos}]/a/div[2]/span/span")
+                          .text.replace(':-', '').replace(' ', ''))
+
+            if driver.find_elements(By.XPATH, f"//div[1]/div/div[{pos}]/a/div[2]/span/span/sup"):
+                price /= 100
+            prices_dict.update({item_names_list[-1]: price})
+            driver.execute_script("arguments[0].scrollIntoView();",
+                                  driver.find_element(By.XPATH, f"//div[1]/div/div[{pos}]/a"))
+            wait_and_click(driver, f"//div[1]/div/div[{pos}]/a")
+            add_to_cart(driver)
+            driver.back()
+
+        sleep(2)
+
+
