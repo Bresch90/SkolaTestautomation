5bf2d47573ee28d9c697e0f0b60b6944896a5550
Bresch90 <92075441+Bresch90@users.noreply.github.com>
2023-02-16 10:10:49 +0100

    Klantade mig med webdriver, hade inte firefox/edge installerat. Behöver inte drivemanger...

--- Tests för Kjell o Company/test_Kjell.py
+++ Tests för Kjell o Company/test_Kjell.py
@@ -16,24 +16,16 @@ def driver(request):
 
     match browser:
         case "chrome":
-            from webdriver_manager.chrome import ChromeDriverManager
-            from selenium.webdriver.chrome.service import Service
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
+            driver = webdriver.Chrome()
         case "firefox":
-            from webdriver_manager.firefox import GeckoDriverManager
-            from selenium.webdriver.firefox.service import Service
-            driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
-
+            driver = webdriver.Firefox()
         case "edge":
-            from webdriver_manager.microsoft import EdgeChromiumDriverManager
-            from selenium.webdriver.edge.service import Service
-            driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
-
+            driver = webdriver.Edge()
         case _:
             raise ValueError(f"Bad input from --browser variable [{browser}]. Did you misspell it?")
 
     yield driver
+    driver.delete_all_cookies()
     driver.quit()
 
 
@@ -51,12 +43,14 @@ class TestKjell:
         driver.get(HOMEPAGE)
         search_bar = driver.find_element(By.XPATH, '//form/div[1]/input')
         search_bar.send_keys("test", Keys.RETURN)
-        WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.XPATH, "//div[2]/div[1]/div/div[1]/a/div[2]/h3"))
+        # wait for new page to load
+        WebDriverWait(driver, timeout=5).\
+            until(lambda d: d.find_element(By.XPATH, "//div[2]/div[1]/div/div[1]/a/div[2]/h3"))
         assert driver.find_element(By.XPATH, "//h3[contains(., 'Kabeltestare')]")
 
     def test_choose_store(self, driver):
         driver.get(HOMEPAGE)
-        driver.maximize_window()
+        driver.maximize_window()  # menu button has different path if screen is too small.
         self.wait_and_click(driver, "//div[3]/div/button")  # menu button
         self.wait_and_click(driver, "//nav/div/div[9]/div")  # choose store
         self.wait_and_click(driver, "//li[contains(.,'Kalmar')]")  # select store
