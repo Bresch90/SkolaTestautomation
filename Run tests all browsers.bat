fe9793d5452e8231babfc6f1c6f8158762895008
Bresch90 <92075441+Bresch90@users.noreply.github.com>
2023-02-23 15:25:11 +0100

    testar eeen gång till

--- Run tests all browsers.bat
+++ Run tests all browsers.bat
@@ -2,4 +2,4 @@ chcp 1252
 pytest -s --tb=short --browser=chrome .\\"Tests för Kjell o Company"\test_Kjell.py
 pytest -s --tb=short --browser=firefox .\\"Tests för Kjell o Company"\test_Kjell.py
 pytest -s --tb=short --browser=edge .\\"Tests för Kjell o Company"\test_Kjell.py
-pause
+@echo Tests done!
