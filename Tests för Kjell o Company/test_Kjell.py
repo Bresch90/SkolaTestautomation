28cc0beccb3109f469c0611402f56adb2eb2f0f8
Bresch90 <92075441+Bresch90@users.noreply.github.com>
2023-03-03 10:22:58 +0100

    Removed scrollback, added some comments

--- Tests för Kjell o Company/test_Kjell.py
+++ Tests för Kjell o Company/test_Kjell.py
@@ -14,7 +14,7 @@ HOMEPAGE = "https://www.kjell.com/se/"
 BROWSER = ''
 HEADLESS = ''
 logging.basicConfig(level=logging.WARNING)
-DEFAULT_MAX_FAILS = 5
+MAX_FAILS = 5
 MAX_TIMEOUT = 5
 
 
@@ -52,7 +52,7 @@ def driver(request):
         case _:
             raise ValueError(f"Bad input from --browser variable [{BROWSER}]. Did you misspell it?")
     driver.get(HOMEPAGE)
-    #TODO fails to find/press accept sometimes?
+    # TODO fails to find/press accept sometimes?
     wait_and_click(driver, "//span[text()='Acceptera']/..")  # accept cookies as it obscures some elements
     WebDriverWait(driver, timeout=MAX_TIMEOUT).until(
         ec.invisibility_of_element((By.XPATH, "//span[text()='Acceptera']/..")))
@@ -61,9 +61,8 @@ def driver(request):
     driver.quit()
 
 
-def wait_and_click(active_driver, path, center_scroll=True, max_fails=DEFAULT_MAX_FAILS):
+def wait_and_click(active_driver, path, center_scroll=True, max_fails=MAX_FAILS):
     tries = 0
-    scroll_value = -650
     while True:
         try:
             # added scrolling to bottom, seemed to fix some elements never loading?
@@ -71,13 +70,12 @@ def wait_and_click(active_driver, path, center_scroll=True, max_fails=DEFAULT_MA
             # wait for element to be available if needed.
             element = WebDriverWait(active_driver, timeout=MAX_TIMEOUT).until(
                 ec.element_to_be_clickable((By.XPATH, path)))
+
+            # ActionChains(active_driver).move_to_element(element).click().perform()  # works without firefox
             # move_to_element action doesn't scroll on firefox, had to use javascript instead.
             active_driver.execute_script("arguments[0].scrollIntoView(true);", element)
             if center_scroll:
-                active_driver.execute_script(f"window.scrollBy(0, {scroll_value});")  # center on screen after scroll.
-                # variate scroll back, see if that fixes "ImBox chat launcher" intercepts
-                # scroll_value += 50
-            # ActionChains(active_driver).move_to_element(element).click().perform()  # works without firefox
+                active_driver.execute_script(f"window.scrollBy(0, -650);")  # center on screen after scroll.
 
             # added javascript click as last try, haven't seen it work yet though...
             if tries > max_fails-1:
@@ -87,6 +85,7 @@ def wait_and_click(active_driver, path, center_scroll=True, max_fails=DEFAULT_MA
                                                                             )))
                 logging.warning("MADE CLICK WITH JAVASCRIPT AS LAST TRY!")
             else:
+                # if it's not the last try, click normally
                 WebDriverWait(active_driver, timeout=MAX_TIMEOUT).until(
                     ec.element_to_be_clickable((By.XPATH, path))).click()
             return
@@ -110,9 +109,8 @@ def wait_and_click(active_driver, path, center_scroll=True, max_fails=DEFAULT_MA
             sleep(1)
 
 
-def wait_and_get_element(active_driver, path, center_scroll=True, max_fails=DEFAULT_MAX_FAILS):
+def wait_and_get_element(active_driver, path, center_scroll=True, max_fails=MAX_FAILS):
     tries = 0
-    scroll_value = -650
     while True:
         try:
             # added scrolling to bottom, seemed to fix some elements never loading?
@@ -123,7 +121,7 @@ def wait_and_get_element(active_driver, path, center_scroll=True, max_fails=DEFA
             # move_to_element action doesn't scroll on firefox, had to use javascript instead.
             active_driver.execute_script("arguments[0].scrollIntoView(true);", element)
             if center_scroll:
-                active_driver.execute_script(f"window.scrollBy(0, {scroll_value});")  # center on screen after scroll.
+                active_driver.execute_script(f"window.scrollBy(0, -650);")  # center on screen after scroll.
                 # variate scroll back, see if that fixes being flaky
                 # scroll_value += 50
             # need to fetch element again since the page destroys some elements when scrolling.
@@ -169,13 +167,12 @@ class TestKjell:
         wait_and_click(driver, "//li[contains(.,'Kalmar')]")  # select store
         wait_and_click(driver, "//button[@data-test-id='choose-store-button']", center_scroll=False)  # accept store
         # ec.invisibility_of_element_located((By.XPATH, "//div[@class='m5']"))  # wait for menu to closed
-        # driver.execute_script("arguments[0].click()", driver.find_element(By.XPATH, "//button[@data-test-id='main-menu-button']"))
 
         sleep(1)
         wait_and_click(driver, "//button[@data-test-id='main-menu-button']", center_scroll=False)  # menu button
         # click_menu_retry(driver)
         # check chosen store
-        #TODO this element cant be found often, menu button fail to press? without sleep its even worse...
+        # TODO this element cant be found often, menu button fail to press? without sleep its even worse...
         chosen_store = wait_and_get_element(driver,
                                             "//div[@data-test-id='my-store-button']/div/div[2]", center_scroll=False)
         assert "kalmar" in chosen_store.text.lower()
@@ -251,7 +248,7 @@ class TestKjell:
                 # last one is because some elements behave wierd with path to sup...for example:
                 # https://www.kjell.com/se/produkter/el-verktyg/matinstrument/matsladdar-prober-kontakter/matsladdar/matsladdar-30-v-3-pack-p37842
                 price /= 100
-                logging.info(f"Found sup! for {name}")
+                logging.info(f"Found sup for {name}")
             else:
                 logging.info(f"Didn't find sup for {name}")
 
